da1c3dbdf503dacf97f4211a4020b28e76f3faef
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-07-13 23:45:51 +0700

    connect db

---
+++ DAO/MSSQLConnect.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class SqlConnectionData
+    {
+        public static SqlConnection Connect()
+        {
+            string strcon = @"Data Source=LAPTOP-AEI9M0MI\\WIZARDSC;Initial Catalog=QuanLySinhVienC#;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(strcon);
+            return conn;
+        }
+    }
+    
+    public class MSSQLConnect
+    {
+        
+    }
+}
