e46d073f24c36baf1273454073d059ebb3c2fd72
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-07-14 00:09:26 +0700

    update GUI

--- GUI/DangNhapGUI.Designer.cs
+++ GUI/DangNhapGUI.Designer.cs
@@ -28,43 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            pictureBox3 = new PictureBox();
             button1 = new Button();
-            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.icons8_twitter_144;
+            pictureBox1.Location = new Point(100, 35);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(87, 71);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semilight", 21.75F, FontStyle.Italic, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(0, 117, 214);
+            label1.Location = new Point(44, 121);
+            label1.Name = "label1";
+            label1.Size = new Size(179, 40);
+            label1.TabIndex = 1;
+            label1.Text = "ĐĂNG NHẬP";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.icons8_user_50;
+            pictureBox2.Location = new Point(24, 204);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(25, 25);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 117, 214);
+            panel1.Location = new Point(24, 247);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(236, 1);
+            panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(0, 117, 214);
+            panel2.Location = new Point(24, 319);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(236, 1);
+            panel2.TabIndex = 6;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.icons8_lock_50;
+            pictureBox3.Location = new Point(24, 277);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(25, 25);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 5;
+            pictureBox3.TabStop = false;
             // 
             // button1
             // 
-            button1.Location = new Point(120, 103);
+            button1.BackColor = Color.FromArgb(0, 117, 214);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Cambria", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(24, 346);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(164, 235);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            button1.Size = new Size(236, 37);
+            button1.TabIndex = 7;
+            button1.Text = "ĐĂNG NHẬP";
+            button1.UseVisualStyleBackColor = false;
             // 
             // DangNhapGUI
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button2);
+            BackColor = Color.White;
+            ClientSize = new Size(292, 447);
             Controls.Add(button1);
+            Controls.Add(panel2);
+            Controls.Add(pictureBox3);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "DangNhapGUI";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "DangNhapGUI";
+            Load += DangNhapGUI_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
+        private PictureBox pictureBox1;
+        private Label label1;
+        private PictureBox pictureBox2;
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox3;
         private Button button1;
-        private Button button2;
     }
 }
