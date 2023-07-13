d704cb4fbbe8b033585700095aa1cf8efd2e702a
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-07-13 23:05:06 +0700

    update DTO

---
+++ DTO/QuyenDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    internal class QuyenDTO
+    {
+        public int maQuyen { get; set; }
+        public int tenQuyen { get; set; }   
+    }
+}
