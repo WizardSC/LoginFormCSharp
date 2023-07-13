ba64253b0d9ebd6f718ec1d20c2ba2dd46d979e9
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-07-14 00:20:50 +0700

    done LoginForm

--- GUI/DangNhapGUI.Designer.cs
+++ GUI/DangNhapGUI.Designer.cs
@@ -34,7 +34,11 @@
             panel1 = new Panel();
             panel2 = new Panel();
             pictureBox3 = new PictureBox();
-            button1 = new Button();
+            btnDangNhap = new Button();
+            label2 = new Label();
+            btnThoat = new Label();
+            txtTenDangNhap = new TextBox();
+            txtMatKhau = new TextBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
@@ -43,7 +47,7 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.icons8_twitter_144;
-            pictureBox1.Location = new Point(100, 35);
+            pictureBox1.Location = new Point(109, 35);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(87, 71);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
@@ -53,11 +57,11 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI Semilight", 21.75F, FontStyle.Italic, GraphicsUnit.Point);
+            label1.Font = new Font("Segoe UI Semibold", 21.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
             label1.ForeColor = Color.FromArgb(0, 117, 214);
-            label1.Location = new Point(44, 121);
+            label1.Location = new Point(58, 119);
             label1.Name = "label1";
-            label1.Size = new Size(179, 40);
+            label1.Size = new Size(186, 40);
             label1.TabIndex = 1;
             label1.Text = "ĐĂNG NHẬP";
             // 
@@ -74,50 +78,97 @@
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(0, 117, 214);
-            panel1.Location = new Point(24, 247);
+            panel1.Location = new Point(24, 235);
             panel1.Name = "panel1";
-            panel1.Size = new Size(236, 1);
+            panel1.Size = new Size(260, 1);
             panel1.TabIndex = 4;
+            panel1.Paint += panel1_Paint;
             // 
             // panel2
             // 
             panel2.BackColor = Color.FromArgb(0, 117, 214);
-            panel2.Location = new Point(24, 319);
+            panel2.Location = new Point(24, 300);
             panel2.Name = "panel2";
-            panel2.Size = new Size(236, 1);
+            panel2.Size = new Size(260, 1);
             panel2.TabIndex = 6;
             // 
             // pictureBox3
             // 
             pictureBox3.Image = Properties.Resources.icons8_lock_50;
-            pictureBox3.Location = new Point(24, 277);
+            pictureBox3.Location = new Point(24, 269);
             pictureBox3.Name = "pictureBox3";
             pictureBox3.Size = new Size(25, 25);
             pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox3.TabIndex = 5;
             pictureBox3.TabStop = false;
             // 
-            // button1
+            // btnDangNhap
             // 
-            button1.BackColor = Color.FromArgb(0, 117, 214);
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Font = new Font("Cambria", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            button1.ForeColor = Color.White;
-            button1.Location = new Point(24, 346);
-            button1.Name = "button1";
-            button1.Size = new Size(236, 37);
-            button1.TabIndex = 7;
-            button1.Text = "ĐĂNG NHẬP";
-            button1.UseVisualStyleBackColor = false;
+            btnDangNhap.BackColor = Color.FromArgb(0, 117, 214);
+            btnDangNhap.FlatAppearance.BorderSize = 0;
+            btnDangNhap.FlatStyle = FlatStyle.Flat;
+            btnDangNhap.Font = new Font("Cambria", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            btnDangNhap.ForeColor = Color.White;
+            btnDangNhap.Location = new Point(24, 353);
+            btnDangNhap.Name = "btnDangNhap";
+            btnDangNhap.Size = new Size(260, 37);
+            btnDangNhap.TabIndex = 7;
+            btnDangNhap.Text = "ĐĂNG NHẬP";
+            btnDangNhap.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.FromArgb(0, 117, 214);
+            label2.Location = new Point(174, 325);
+            label2.Name = "label2";
+            label2.Size = new Size(110, 16);
+            label2.TabIndex = 8;
+            label2.Text = "Xóa các trường";
+            // 
+            // btnThoat
+            // 
+            btnThoat.AutoSize = true;
+            btnThoat.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btnThoat.ForeColor = Color.FromArgb(0, 117, 214);
+            btnThoat.Location = new Point(131, 403);
+            btnThoat.Name = "btnThoat";
+            btnThoat.Size = new Size(47, 16);
+            btnThoat.TabIndex = 9;
+            btnThoat.Text = "Thoát";
+            // 
+            // txtTenDangNhap
+            // 
+            txtTenDangNhap.BorderStyle = BorderStyle.None;
+            txtTenDangNhap.Font = new Font("Cambria", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            txtTenDangNhap.Location = new Point(58, 204);
+            txtTenDangNhap.Multiline = true;
+            txtTenDangNhap.Name = "txtTenDangNhap";
+            txtTenDangNhap.Size = new Size(226, 25);
+            txtTenDangNhap.TabIndex = 10;
+            // 
+            // txtMatKhau
+            // 
+            txtMatKhau.BorderStyle = BorderStyle.None;
+            txtMatKhau.Font = new Font("Cambria", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            txtMatKhau.Location = new Point(58, 269);
+            txtMatKhau.Multiline = true;
+            txtMatKhau.Name = "txtMatKhau";
+            txtMatKhau.Size = new Size(226, 25);
+            txtMatKhau.TabIndex = 11;
             // 
             // DangNhapGUI
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(292, 447);
-            Controls.Add(button1);
+            ClientSize = new Size(310, 447);
+            Controls.Add(txtMatKhau);
+            Controls.Add(txtTenDangNhap);
+            Controls.Add(btnThoat);
+            Controls.Add(label2);
+            Controls.Add(btnDangNhap);
             Controls.Add(panel2);
             Controls.Add(pictureBox3);
             Controls.Add(panel1);
@@ -144,6 +195,10 @@
         private Panel panel1;
         private Panel panel2;
         private PictureBox pictureBox3;
-        private Button button1;
+        private Button btnDangNhap;
+        private Label label2;
+        private Label btnThoat;
+        private TextBox txtTenDangNhap;
+        private TextBox txtMatKhau;
     }
 }
